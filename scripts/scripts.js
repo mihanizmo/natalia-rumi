66c60525fb9e9469fedaa52849f1262e87c62a9e
mihanizmo <mihanizmo@mail.ru>
2018-12-18 21:49:18 +0800

    Работа с блоком last-work

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,92 +1,92 @@
-$('header').addClass('animated fadeIn');
+//$('b-header').addClass('animated fadeIn');
 
-$('nav .logo').addClass('animated bounceInLeft delay-1s');
+$('.b-logo').addClass('animated bounceInLeft delay-1s');
 
-$('.navigation').addClass('animated bounceInRight delay-1s');
+$('.b-navigation').addClass('animated bounceInRight delay-1s');
 
 $('.carousel-start').addClass('animated bounceIn delay-1s');
 
-$(document).ready(function () {
-    $('.last-header').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeIn",
-        offset: 300
-    });
-
-    $('.last-img-1').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeInLeft",
-        offset: 300
-    });
-
-    $('.last-img-2').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeInUp",
-        offset: 300
-    });
-
-    $('.last-img-3').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeInRight",
-        offset: 300
-    });
-
-    $('.last-img-4').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeInLeft",
-        offset: 300
-    });
-
-    $('.last-img-5').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeInUp",
-        offset: 300
-    });
-
-    $('.last-img-6').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeInRight",
-        offset: 300
-    });
-
-    $('.comment h1').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated flipInX",
-        classToRemove: "hidden-vpc",
-        offset: 300
-    });
-
-    $('.offer-header').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeIn",
-        offset: 300
-    });
-
-    $('.offer-card').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeInUp",
-        offset: 300
-    });
-
-    $('.comment-back h1').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated flipInX",
-        classToRemove: "hidden-vpc",
-        offset: 200
-    });
-
-    $('.insta-header').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated fadeIn",
-        offset: 300
-    });
-
-    $('.insta-card').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc magictime puffIn",
-        offset: 500
-    });
-
-    $('.insta-card a').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc magictime puffIn",
-        offset: 200
-    });
-
-    $('.call h1').addClass("hidden-vpc").viewportChecker({
-        classToAdd: "visible-vpc animated rollIn",
-        offset: 200
-    });
-    
-    $('.call p').viewportChecker({
-        classToAdd: "animated tada delay-2s",
-        offset: 200
-    });
-});
+//$(document).ready(function () {
+//    $('.last-header').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeIn",
+//        offset: 300
+//    });
+//
+//    $('.last-img-1').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeInLeft",
+//        offset: 300
+//    });
+//
+//    $('.last-img-2').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeInUp",
+//        offset: 300
+//    });
+//
+//    $('.last-img-3').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeInRight",
+//        offset: 300
+//    });
+//
+//    $('.last-img-4').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeInLeft",
+//        offset: 300
+//    });
+//
+//    $('.last-img-5').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeInUp",
+//        offset: 300
+//    });
+//
+//    $('.last-img-6').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeInRight",
+//        offset: 300
+//    });
+//
+//    $('.comment h1').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated flipInX",
+//        classToRemove: "hidden-vpc",
+//        offset: 300
+//    });
+//
+//    $('.offer-header').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeIn",
+//        offset: 300
+//    });
+//
+//    $('.offer-card').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeInUp",
+//        offset: 300
+//    });
+//
+//    $('.comment-back h1').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated flipInX",
+//        classToRemove: "hidden-vpc",
+//        offset: 200
+//    });
+//
+//    $('.insta-header').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated fadeIn",
+//        offset: 300
+//    });
+//
+//    $('.insta-card').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc magictime puffIn",
+//        offset: 500
+//    });
+//
+//    $('.insta-card a').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc magictime puffIn",
+//        offset: 200
+//    });
+//
+//    $('.call h1').addClass("hidden-vpc").viewportChecker({
+//        classToAdd: "visible-vpc animated rollIn",
+//        offset: 200
+//    });
+//    
+//    $('.call p').viewportChecker({
+//        classToAdd: "animated tada delay-2s",
+//        offset: 200
+//    });
+//});
 
